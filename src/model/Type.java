f79a3c47142f38e78c39e9bcd68b3e53f7e7a46e
petosorus <petosorus@hotmail.fr>
2014-01-01 22:46:11 +0100

    Type abstrait

--- src/model/Type.java
+++ src/model/Type.java
@@ -8,9 +8,14 @@ public abstract class Type{
 	Set<Type> strong;
 
 	public double coefficient(Type attacked){
-		if(noEffect.contains(attacked)){ return 0;}
-		if(weak.contains(attacked)){ return 0.5;}
-		if(strong.contains(attacked)){ return 2;}
+		if(noEffect.contains(attacked)){ return 0; }
+		if(weak.contains(attacked)){ return 0.5; }
+		if(strong.contains(attacked)){ return 2; }
+		return 1;
+	}
+	
+	public double coefficientSTAB(Type attacker){
+		if(this.equals(attacker)){ return 1.5; }
 		return 1;
 	}
 }
