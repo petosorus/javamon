0dd138b15020635265c1b8ae2e7a3721a170d899
petosorus <petosorus@hotmail.fr>
2014-01-05 03:14:35 +0100

    Attaque et niveau de modif de stat

--- src/model/Attack.java
+++ src/model/Attack.java
@@ -17,16 +17,19 @@ public class Attack extends Ability{
 	
 	protected int damageCalculation(Pokemon attacker, Pokemon defender){
 		int level = attacker.level;
-		int attack = attacker.attack.value;
+		int attack = attacker.attack.curValue;
 		double power = force * type.coefficientSTAB(attacker.type1) *
 			type.coefficientSTAB(attacker.type2);
-		int defense = attacker.defense.value;
+		int defense = attacker.defense.curValue;
 		double coefficient = type.coefficient(defender.type1) * 
 			type.coefficient(defender.type2);
 		
-		
 		return (int)((((level * 0.4 + 2) * attack * power)/(defense * 50) +2) *
 			coefficient);
+	}
+	
+	public void attack(Pokemon attacker, Pokemon defender){
+		defender.decreaseHP(damageCalculation(attacker, defender));
 	}
 	
 }
--- src/model/Statistic.java
+++ src/model/Statistic.java
@@ -4,14 +4,50 @@ public class Statistic{
 	
 	int value;
 	int curValue;
+	int statLevel;
 	
 	public Statistic(int value){ this.value = value; }
 	
 	public void increment(int value){ this.value += value; }
 	
-	public void tempDecrement(int value){ curValue -= value; }
+	public void tempDecrement(int value){ statLevel -= value; }
 	
-	public void tempIncrement(int value){ curValue += value; }
+	public void tempIncrement(int value){ statLevel += value; }
 	
-	public void resetTempCondition(){ curValue = value; }
+	public void resetTempCondition(){ 
+		curValue = value;
+		statLevel = 0;
+	}
+	
+	public void adaptStat(){
+		switch(statLevel){
+			case -6: curValue = 25;
+    				 break;
+    		case -5: curValue = 29;
+    				 break;
+    		case -4: curValue = 33;
+    				 break;
+    		case -3: curValue = 40;
+    				 break;
+    		case -2: curValue = 50;
+    				 break;
+    		case -1: curValue = 67;
+    				 break;
+    		case 0: curValue = 100;
+    				 break;
+    		case 1: curValue = 150;
+    				 break;
+    		case 2: curValue = 200;
+    				 break;
+    		case 3: curValue = 250;
+    				 break;
+    		case 4: curValue = 300;
+    				 break;
+    		case 5: curValue = 350;
+    				 break;
+    		case 6: curValue = 400;
+    				 break;
+    	}
+    }
+	
 }
